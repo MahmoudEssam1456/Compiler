568877d7a1cb72fe587e8e83d80d8af2c04bc898
Mahmoud Essam <mahmoudessam1456@gmail.com>
2019-01-02 18:21:55 +0200

    update

--- main.c
+++ main.c
@@ -3,23 +3,23 @@
 #include <string.h>
 #include <ctype.h>
 
-// Function for checking whether it's a keyword or not !
 static FILE *source_fp, *dest_fp;
 
+// <Token-Type,Token-Value>
 struct pair {
     char* label;
-    char value[20]; // maximum number of accepted word length is 20
+    char value[20]; // Maximum number of accepted word length is 20
 };
 
-struct pair pairs[5][30];
-int k=0,l=0; // for the array of structs (pairs)
-int bufferCnt = 0; // Buffer Counter for concatenating chars and cutting them
+struct pair pairs[10][30];
+int k = 0, l = 0;           // For the array of structs (pairs)
+int bufferCnt = 0;          // Buffer Counter for concatenating chars and cutting them
 
+
+/* ....................... Checking if a given word is a KeyWord or not .....................*/ 
 int isKeyword(char buffer[]){
 
-    // Samy Code of manipulating the keywords or adding some of them below .. ( PYTHON )
-
-    char keywords[33][10] = {"False", "class", "finally", "is", "return", "None", "continue",
+    char keywords[38][10] = {"int", "long", "float", "string", "False","double", "class", "finally", "is", "return", "None", "continue",
                              "for", "lambda", "try", "and", "del", "global", "not", "with",
                              "True", "def", "from", "nonlocal", "while", "as", "elif",
                              "if", "or", "yield", "assert", "else", "import",
@@ -41,8 +41,9 @@ int isKeyword(char buffer[]){
     return flag;
 }
 
-// remove end of line
-void remove_end_of_line(char line[]){
+
+/* ....................... Removing End of Line .....................*/ 
+void removeEndofLine(char line[]){
     int i = 0;
     while (1)
     {
@@ -54,7 +55,7 @@ void remove_end_of_line(char line[]){
     line[i] = '\0';
 }
 
-// check for extension we accept .hapd only
+/* ....................... Checking the file Extension (.Hapd) .....................*/ 
 void extention(const char path[]){
     int i = 0;
     char check[5];
@@ -69,18 +70,17 @@ void extention(const char path[]){
             check[4] = '\0';
             if (strcmp(check, "hapd") != 0)
             {
-                printf("error in your file extension, your file extension is %s ,please make sure to make it .hapd\n", check);
+                printf("Error in your file extension, your file extension is %s ,please make sure to make it .hapd\n", check);
                 exit(0);
             }
             else
-
                 break;
         }
         i++;
     }
 }
 
-/* added for operators (Salim) */
+/* ....................... Checking - Single Operators - .....................*/ 
 int isOp(char *c){
     char operators[] = "+-*/%=><!";
     int i = 0;
@@ -92,61 +92,74 @@ int isOp(char *c){
     return -1;
 }
 
-char* return_path(){
-    // Nassar Code of reading the path of the file and checking it's extension
-
+/* ....................... Handling the file Path and format .....................*/ 
+char* returnPath(){
     FILE *fp;
     // Enter PATH of file which want to compile
     char PATH[100];
     printf("Enter Full PAth to your file\n");
     fgets(PATH, 100, stdin);
-    remove_end_of_line(PATH); // remove \n from input file
+    removeEndofLine(PATH); // remove \n from input file
     extention(PATH);
-
-
     return PATH;
 }
 
-void init_io(FILE **fp, FILE *std, const char mode[], const char fn[]) {
+/* ....................... Handling Inputs (.Hapd/Path/StdIn) .....................*/ 
+void initIo(FILE **fp, FILE *std, const char mode[], const char fn[]) {
     if (fn[0] == '\0')
         *fp = std;
     else if(fn[0] == '-' && fn[1] == 'p'){
-        //from a certain given path
-        fn = return_path();                  // File Pointer to open the file needed..
+        // From a certain given path
+        fn = returnPath();                  // File Pointer to open the file needed..
         if ((*fp = fopen(fn, mode)) == NULL)
             printf("Failed to open file");
     }
     else{
-        //from argv (in the same folder)
+        // From argv (in the same folder)
         extention(fn);
         if ((*fp = fopen(fn, mode)) == NULL)
             printf("Failed to open file");
     }
 }
 
-void checkKwdOrIdnt(char buffer[]){
+/* ....................... Checking for a KeyWord or Identfier .....................*/ 
+void isKeyIdnt(char buffer[]){
+    char doubleOperators[6][5] = {"gr", "sm", "greq", "smeq", "eq", "neq"};
     if (buffer[0] == '\0'){}
     else if (isKeyword(buffer) == 1){
     	pairs[k][l] = (struct pair){"keyword","\0"};
         strcpy(pairs[k][l].value, buffer);
         l++;
-       // printf("%s is keyword\n", buffer);
     }
     else if (isKeyword(buffer) == 0){
+        int i, op_flag = 0;
+        for(i =0; i < 6; i++)
+        {
+            if(!strcmp(buffer, doubleOperators[i])){
+            pairs[k][l] = (struct pair){"operator","\0"};
+            strcpy(pairs[k][l].value, buffer);
+            l++;
+            op_flag = 1;
+            break;
+            }
+        }
+        if(op_flag == 0)
+        {
     	pairs[k][l] = (struct pair){"identifier","\0"};
         strcpy(pairs[k][l].value, buffer);
         l++;
-      //  printf("%s is indentifier\n", buffer);
+        }
     }
 }
 
+/* ....................... Checking for - Double Operators - .....................*/ 
 void checkOperators (char *ch, char buffer[]){
 
-    int first = 0; // to catch the operators
+    int first = 0; // To catch the operators
     first = isOp(ch);
     if (first == 1)   //first char is operator
     {
-    	checkKwdOrIdnt(buffer);
+    	isKeyIdnt(buffer);
         bufferCnt = 0;
         char ch2 = fgetc(source_fp);
         int second = isOp(&ch2);
@@ -166,11 +179,6 @@ void checkOperators (char *ch, char buffer[]){
                     pairs[k][l] = (struct pair){"double operator",""};  /// check with strcat()
                     strcpy(pairs[k][l].value, op);
                     l++;
-                   // printf("%c%c is the double operator\n", *ch, ch2); // here we have the double operators
-                }
-                else
-                {
-                   // printf("There is no such operator as: %c%c\n", *ch, ch2);
                 }
                 *ch = ch3;
             }
@@ -179,13 +187,12 @@ void checkOperators (char *ch, char buffer[]){
         {
         	pairs[k][l] = (struct pair){"single operator",*ch};
         	l++;
-           // printf("%c is the single operator\n", *ch); // here we have the double operator
             *ch = ch2;
         }
     }
 }
 
-
+/* ....................... Checking for - Special Characters - .....................*/ 
 void checkSpecial(char *ch, char buffer[]){
     int i;
     char special[13]="[]{},.();:'\"\?";
@@ -196,37 +203,33 @@ void checkSpecial(char *ch, char buffer[]){
             if (bufferCnt != 0)
             {
             	bufferCnt = 0;
-                checkKwdOrIdnt(buffer);
+                isKeyIdnt(buffer);
             }
             pairs[k][l] = (struct pair){"special character",*ch};
             l++;
-            //printf("%c is special character\n", *ch);
         }
     }
 }
 
-
-void check_all(){
+/* ....................... Checking for - Special Characters - .....................*/ 
+void checkAll(){
     char ch;
     char buffer[20];
     int i;
     while ((ch = fgetc(source_fp)) != EOF)
     {
-
         checkOperators(&ch, buffer);
-
         checkSpecial(&ch, buffer);
-
         if (isalnum(ch))
     	{
         	buffer[bufferCnt++] = ch;
-        	buffer[bufferCnt] = '\0';  // added this to modify the buffer dynamically
+        	buffer[bufferCnt] = '\0';  // Added this to modify the buffer dynamically
     	}
         if ((ch == ' ' || ch == '\n' ) && (bufferCnt != 0))
         {
-            checkKwdOrIdnt(buffer);
+            isKeyIdnt(buffer);
             bufferCnt = 0;
-            buffer[bufferCnt] = '\0';  // to check with it in the checkKwdOrIdnt for nulls
+            buffer[bufferCnt] = '\0';  // To check with it in the isKeyIdnt for nulls
         }
 
         if(ch=='\n'){
@@ -240,69 +243,41 @@ void check_all(){
 
 
     }
-    if(bufferCnt != 0)checkKwdOrIdnt(buffer);
+    if(bufferCnt != 0)isKeyIdnt(buffer);
     int j=0;
-/*
-    for (i = 0; i < 5; i++){
-        while(pairs[i][j].label != '\0'){
-    		printf("%s: %s\n",  pairs[i][j].label, pairs[i][j].value);
-            j++;
-        }
-        j=0;
-    }*/
 }
-char a[40][20];
-int top=-1;
 
-void error(){
-printf("Syntax Error");
+char a[40][20];
+int  top = -1;
+
+
+void syntaxError(){
+printf("\nSyntax Error\n\n");
 }
+
+
 void push(char k[])
 {
     int i = 0;
-    //char m[30];
-    //printf("current top=%d\n\n",top);
-    //printf("m=%s\n",m);
     char* args[10];
-    args[i] = strtok(k," ");
-    //printf("args[0]=%s\n",args[i]);
+    args[i] = strtok(k," ");    // K represents the rule
     while(args[i] != NULL){
         i++;
         args[i] = strtok(NULL," ");
-        //printf("args[%d]=%s\ti=[%d]\n",i,args[i],i);
     }
-    printf("\n");
-    //printf("args=%s\n",args[i-1]);
-    for(i=i-1;i>=0;i--){
+
+    for(i = i-1; i >= 0; i--){
         top++;
-        strcpy(a[top],args[i]);
-        //printf("a[%d]=\targs[%d]=%s\n",top,i,args[i]);
-        //printf("a[%d]=%s\n",top,a[top]);
+        strcpy(a[top], args[i]);
     }
-    printf("\n");
-    for(i=0;i<=top;i++){
-        printf("a[%d]=%s\n",i,a[i]);
-    }
-    printf("\n");
 }
-char* TOS()        //Returns TOP of the Stack
-{
+char* TOS(){        //Returns TOP of the Stack
   return a[top];
 }
-void pop()       //Pops 1 element from the Stack
-{
 
-  //printf("oldtop=%d\n",top);
+void pop(){       //Pops 1 element from the Stack
   strcpy(a[top],"\0");
   top--;
-  //printf("newtop=%d\n",top);
-  //printf("top=%s\n",a[top]);
-}
-void display()  //Displays Elements Of Stack
-{
-  int i;
-  for(i=0;i<=top;i++)
-    printf("%s",a[i]);
 }
 
   char non_terminal[31][5]={"S","D","AT","ST","CT","BT","DSC","AS","X","ET","ARE","A","AP","B","DP","C","SE","BE","ALE","DSE","DT","EX","IFE","CON","V","OP","LE","WE","FE","I","IC"};
@@ -339,76 +314,74 @@ void display()  //Displays Elements Of Stack
   "$","$","$","$","$","$","$","$","$","$","$","identifier","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$",
   "$","$","$","$","$","$","$","$","$","$","$","identifier","$","$","$","$","$","$","$","number","( IC )","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$","$"
   };
+
 int main(int argc, char *argv[]){
 
-    // get data from stdin or path or from argv
-    init_io(&source_fp, stdin,  "r",  argc > 1 ? argv[1] : "");
-    int i=0,j=0,ir=0,ic=0,k;
+    printf("\nIf you want to use the stdIn - Compiler the file then write your code \n");
+    printf("then press Ctrl + D , if you want to use a textFile Hint: Must be ./a.out TextName.Hapd and in the same Directory \n");
+    printf("if the file .Hapd in other directory just compile then ./a.out -p and the full path. \n \n");
+    
+    initIo(&source_fp, stdin,  "r",  argc > 1 ? argv[1] : "");
+    int i = 0, j = 0, ir = 0, ic = 0, choice = 0;
     char* term,*nonterm,r[30];
-    check_all();
+    checkAll();
+    while(1){
 
+    
+    printf("Choose what you want: Press (1 or 2) \n");
+    printf("1- Lexical Analysis \n");
+    printf("2- Parsing LL(1) \n \n");
+    scanf("%d", &choice);
+    // Lexical Analysis Phase
+    if(choice == 1)
+    {
+        for (i = 0; i < k; i++){
+            while(pairs[i][j].label != '\0'){
+    		printf("%s: %s\n",  pairs[i][j].label, pairs[i][j].value);
+            j++;
+            }
+            j = 0;
+        }
+        printf("\n");
+    }
+    // Parsing Phase
+    else if (choice == 2){
     char line[10]="S $";
     push(line);
-
-    for (i = 0; i <= 3; i++){
+    for (i = 0; i < k; i++){
+        printf("%d", k);
         while(pairs[i][j].label != '\0'){
             int n=0;
-    		printf("%s: %s\n",  pairs[i][j].label, pairs[i][j].value);
             if(!strcmp(TOS(),term)||!strcmp(TOS(),"e"))
                 {
                     pop();
                     j++;
                     term=pairs[i][j].value;
                     if(!strcmp(term,"else"))
-                        strcpy(term,"else:");
-                    printf("term=%s",term);
-                    //printf("pair=%s\n",pairs[i][j+1].value);
-                    //if(!strcmp(pairs[i][j+1].value,"'")){
-                    //j++;
-                    //printf("pair2=%s\n",pairs[i][j].value);}
+                    strcpy(term,"else:");
                 }
-            /*
-            if(!strcpy(pairs[i][j].value,"'")&&strcpy(pairs[i][j+2].value,"'")){
-                j++;
-                printf("here55");
-            }*/
+
             else if(strcmp(TOS(),"$")){
                 term=pairs[i][j].value;
                 if(!strcmp(term,"else"))
                         strcpy(term,"else:");
-                printf("term=%s\n",term);
                 nonterm=TOS();
-                //printf("nonterm=%s\n",nonterm);
                 for (n=0;n<31;n++){
                     if(!strcmp(non_terminal[n],nonterm)){
                         ir=n;
                         break;
                     }
                 }
-                printf("ir=%d\n",ir);
-                /*
-                if(ir!=n){
-                    if(strcmp(nonterm,";")){
-                    //printf("er?\n");
-                    error();
-                    break;
-                    }
-                }*/
-
                 for (n=0;n<42;n++){
                     if(!strcmp(terminal[n],term)){
                         ic=n;
                         break;
                     }
                 }
-                //printf("ic=%d\n",ic);
-
                 if(ic!=n){
-                //printf("label=%s\n",pairs[i][j].label);
                 if(((!strcmp(term,"'")|| term[0]=='"'))&&((!strcmp(pairs[i][j+2].value,"'"))||(!strcmp(pairs[i][j+2].value,"\"")))){
                     j++;
                     term=pairs[i][j].value;
-                    //printf("here\n");
                     if(strlen(term)==1){
                         term="character";
                         pairs[i][j].label="character";
@@ -420,10 +393,8 @@ int main(int argc, char *argv[]){
                         pairs[i][j].label="letters";
                         ic=22;
                     }
-                    printf("term=%s\n",term);
                 }
                 else if((!strcmp(term,"'")|| !strcmp(term,"\""))){
-                    //printf("here2\n");
                     j++;
                     term=pairs[i][j].value;
                     for (n=0;n<42;n++){
@@ -438,19 +409,16 @@ int main(int argc, char *argv[]){
                 {
                     term="character";
                     ic=25;
-                    //printf("newic=%d\n",ic);
                 }
 
                 else if(!strcmp(pairs[i][j].label,"letters"))
                 {
                     term="letters";
                     ic=22;
-                    //printf("newic=%d\n",ic);
                 }
                 else if(term[0]>='0' && term[0]<='9'){
                     int f=1;
                     int len=strlen(term);
-                    printf("len=%lu\n",strlen(term));
 
                     for(n=0;n<len;n++){
                         if(term[n]>='0' && term[n]<='9')
@@ -472,7 +440,6 @@ int main(int argc, char *argv[]){
                 {
                     term="number";
                     ic=19;
-                    //printf("newic=%d\n",ic);
                 }
                 else if(term[0]=='['){
                     j++;
@@ -527,29 +494,22 @@ int main(int argc, char *argv[]){
                 {
                     term="[number]";
                     ic=13;
-                    //printf("newic=%d\n",ic);
                 }
                 else if(!strcmp(pairs[i][j].label,"[number][number]"))
                 {
                     term="[number][number]";
                     ic=14;
-                    //printf("newic=%d\n",ic);
                 }
                 else if(!strcmp(pairs[i][j].label,"identifier"))
                 {
                     term="identifier";
                     ic=11;
-                    //printf("newic=%d\n",ic);
                 }
                 else{
-                    error();
+                    syntaxError();
                     break;
                 }
                 }
-                printf("ic=%d\n",ic);
-                //printf("topbefore=%s\n",TOS());
-                //strcpy(ah,t[ir][ic]);
-               // printf("topbefore=%s\n",TOS());
                 int z=0;
                 for(n=0;n<31;n++){
                     if(!strcmp(nonterm,non_terminal[n])){
@@ -557,35 +517,26 @@ int main(int argc, char *argv[]){
                         break;
                     }
                 }
-                printf("term=%s\n",term);
                 if(z==1){
+                printf("%s ---> ", TOS());
                 pop();
-                //r=t[ir][ic];
                 strcpy(r,t[ir][ic]);
-                //printf("old r=%s\n",r);
-                //printf("table before push=%s\n",t[ir][ic]);
-
+                printf("%s", r);
+                printf("\n");
                 push(r);
                 }
-                //push(t[ir][ic]);                                 //error t[ir][ic] changed
-                //strcpy(t[ir][ic],r);
-                //printf("new r=%s\n",r);
-                //printf("table after push=%s\n",t[ir][ic]);
-
                 if(!strcmp(TOS(),"e"))
                 {
                     pop();
-                    printf("pop\n");
                 }
             }
-            //printf("TOS=%s\nterm=%s\n",TOS(),term);
             if(!strcmp(TOS(),"$")&&!strcmp(term,";")){
-                printf("\n Given String is accepted\n\n");
+                printf("\nGiven String is accepted \n\n");
                 break;
 
             }
             if(!strcmp(TOS(),"$")){
-                error();
+                syntaxError();
                 break;
             }
             }
@@ -599,8 +550,8 @@ int main(int argc, char *argv[]){
             term="";
             j=0;
     }
-
-    fclose(source_fp); // close the file
-
+    }
+    }
+    fclose(source_fp); // close the file}
     return 0;
 }
